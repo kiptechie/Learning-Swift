18e6bd839911f44e6cb4f66b367627c4b63a8798
Timothy Serem <kipkorirserem3@gmail.com>
2023-06-14 23:31:36 +0300

    day one done :100:

--- Playground/Variables.playground/Contents.swift
+++ Playground/Variables.playground/Contents.swift
@@ -1,5 +1,30 @@
 import UIKit
 
+// vars can change over time
 var greeting = "Hello, playground"
-var age = 25
-var population = 40_000_000
+var age = 25 // integer
+var population = 40_000_000 // integer
+var longStr = """
+Hello
+World
+:)
+"""
+var longStrWithoutLineBreaks = """
+Hello \
+World \
+:)
+"""
+var pi = 3.141 // double
+var awesome = true
+var score = 85
+var str = "Your score was \(score)"
+var result = "The result was: \(str)"
+
+// constants use the 'let' keyword
+let taylor = "swift"
+
+// type annotations
+let album: String = "Album"
+let year: Int = 1998
+let height: Double = 1.70
+let short: Bool = true
