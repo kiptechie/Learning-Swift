3b2b73ff7ac77e744e939699ba33b134aab263cc
Timothy Serem <kipkorirserem3@gmail.com>
2023-06-23 00:27:28 +0300

    Day 6

---
+++ Closures/Closures.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let driving = {
+    print("I am driving")
+}
+
+driving()
+
+let cruising = { (place: String) in
+    print("Cruising in the \(place)")
+}
+
+cruising("Sea")
+
+let drivingWithReturn = { (place: String) -> String in
+    return "I am driving in \(place)"
+}
+
+let ans = drivingWithReturn("Ocean")
+
+// closures as parameters
+func travel(action: () -> Void) {
+    action()
+}
+
+travel(action: driving)
+
+travel() {
+    print("I am traveling")
+}
+
+travel {
+    print("I am traveling")
+}
+
+func travelWithParam(action: (String) -> Void) {
+    action("Nairobi")
+}
+
+travelWithParam { (place: String) in
+    print("I am travelling to \(place)")
+}
+
+// return data
+
+func travelWithReturn(action: (String) -> String) {
+    print("I am getting ready to go!")
+    let desc = action("Nairobi")
+    print(desc)
+    print("Arrived")
+}
+
+travelWithReturn { (place: String) -> String in
+    return "I am going to \(place)"
+}
+
+func travelWithSpeed(action: (String, Int) -> String) {
+    let desc = action("Nairobi", 100)
+    print(desc)
+}
+
+travelWithSpeed {
+    return "I am going to \($0) at \($1)Kph"
+}
+
+// returning closures from functions
+func safiri() -> (String) -> Void {
+    var counter = 0
+    return {
+        print("I am going to \($0)")
+        counter += 1
+        print("Counter: \(counter)")
+    }
+}
+
+let res = safiri()
+res("Eldoret")
+res("Eldoret")
+res("Eldoret")
+res("Eldoret")
